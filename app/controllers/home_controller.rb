269180d9c143c48288db91dc2bf11cad17b4de16
Alntjan <oliveiracatarino88@gmail.com>
2016-01-18 15:02:31 +0000

    Start date for Order Query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,7 @@ class HomeController < ApplicationController
   layout 'embedded_app'
 
   def index
-    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 10})
-    @mb = ShopifyAPI::Order.find(:all, :params => { :gateway => "Multibanco" })
+    @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 100, :created_at_min => "2016-01-8 00:00"})
   end
 
 end
