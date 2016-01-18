becdf0d6818dbb288e16e86ecc8e3b2eee4872ea
Alntjan <oliveiracatarino88@gmail.com>
2016-01-18 15:46:13 +0000

    Gateway.multibanco?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     @orders = ShopifyAPI::Order.find(:all, :params => {:limit => 100, :created_at_min => "2016-01-8 00:00"})
     @multibancos = []
     @orders.each do |category|
-      tposts = category.multibanco?
+      tposts = category.gateway.multibanco?
       @multibancos += tposts if tposts
     end
   end
