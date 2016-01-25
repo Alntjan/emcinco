e189b39c37e85f320c8f9b162e57ef7084615b64
Alntjan <oliveiracatarino88@gmail.com>
2016-01-25 00:50:57 +0000

    Precompile Settings

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module TestApp
     config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
